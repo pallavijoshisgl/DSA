7d78a2c1a9bfe1c771363f52f56ac0ed23d0bfad
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-08-19 16:16:58 +0530

    Add program for Generate Document

---
+++ String/Easy/Create Document/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Program
+{
+    public static bool CreateDocument(string characters, string document)
+    {
+        Dictionary<char, int> dic = new Dictionary<char, int>();
+        foreach (var character in characters)
+        {
+            if (!dic.ContainsKey(character))
+            {
+                dic.Add(character, 0);
+            }
+            dic[character]++;
+        }
+
+        foreach (var ch in document)
+        {
+            if (!dic.ContainsKey(ch) || dic[ch] == 0)
+            {
+                return false;
+            }
+            dic[ch]--;
+
+        }
+        return true;
+    }
+
+    public static void Main(string[] args)
+    {
+        bool result = CreateDocument("xghjklwexvbnm", "vewxcbn");
+        Console.WriteLine(result);
+    }
+}
