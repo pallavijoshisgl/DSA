4a567df52da105468b0641fc34b6537cea815bf8
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-08-20 16:57:14 +0530

    Add solution for Run-Length Encoding problem

---
+++ String/Easy/Run-Length Encoding/ConsoleApp1/Run_Length_Encoding.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+
+public class Run_Length_Encoding
+{
+    public static string RunLengthEncoding(string s)
+    {
+        int currentLength = 1;
+        string result = "";
+
+        for(int i=1;i<s.Length;i++)
+        {
+            char currentCharacter = s[i];
+            char previousCharacter = s[i - 1];
+
+            if(currentCharacter!=previousCharacter || currentLength==9)
+            {
+                result+=currentLength.ToString();
+                result += previousCharacter.ToString();
+                currentLength = 0;
+            }
+
+            currentLength += 1;
+        }
+
+        result += currentLength.ToString();
+        result += s[s.Length - 1].ToString();
+
+        return result;
+
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(RunLengthEncoding("AAAAAAAAAAAAAABBBCCDD"));
+    }
+}
