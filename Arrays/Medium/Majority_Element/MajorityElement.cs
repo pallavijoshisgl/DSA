2965a76235d1d1535dfbf68c32e961bd319500a3
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-08-21 22:59:29 +0530

    Add solution for Majority Element Problem

---
+++ Arrays/Medium/Majority_Element/MajorityElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+public class MajorityEelemnt
+{
+    public static int FindMajorityElement(int[] inputArray)
+    {
+        int counter = 0;
+        int majorityElement = 0;
+        for(int i=0; i<inputArray.Length; i++)
+        {
+            if(counter==0)
+            {
+                majorityElement=inputArray[i];
+            }
+           
+            if(inputArray[i] == majorityElement)
+            {
+                counter++;
+            }
+            else
+            {
+                counter--;
+            }
+        }
+        return majorityElement;
+
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] input = new int[] { 1, 2, 3, 4, 2, 5, 2, 2 };
+        int result= FindMajorityElement(input);
+        Console.WriteLine(result);
+    }
+}
