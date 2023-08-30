a32fb87e32a8f0a5c6844e45aada288020b56aea
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-08-30 23:09:45 +0530

    Add solution for problem Split BinaryTree

---
+++ Trees/Medium/Split_Binary_Tree/SplitBinaryTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class SplitBinaryTree
+{
+    public class BinaryTree
+    {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value)
+        {
+            this.value = value;
+        }
+
+
+    }
+
+    public static int SplitsBinaryTree(BinaryTree tree)
+    {
+        var lstTreeSum = new List<int>();
+        var numTreeSum = GetTreeSum(lstTreeSum, tree);
+        if(numTreeSum % 2 !=0)
+        {
+            return 0;
+        }
+        foreach (var sum in lstTreeSum)
+        {
+            if(sum == numTreeSum/2)
+            {
+                return sum;
+            }
+        }
+        return 0;
+    }
+
+    public static int GetTreeSum(List<int> lstTreeSum,BinaryTree tree)
+    {
+        if(tree==null)
+        {
+            return 0;
+        }
+
+        var numLeftSum=GetTreeSum(lstTreeSum, tree.left);
+        var numRightSum= GetTreeSum(lstTreeSum, tree.right);
+        var numTotal = tree.value + numLeftSum + numRightSum;
+        lstTreeSum.Add(numTotal);
+        return numTotal;
+    }
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
+
