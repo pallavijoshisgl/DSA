1875cecae34357324ee3b7515ea78331247a9048
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-08-31 22:25:54 +0530

    Add solution for Find Closest Value BST

---
+++ Trees/Medium/Find_Closest_Value_BST/FindClosestValueBST.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class FindClosestValueBST
+{
+    public static int FindClosestValueInBST(BST tree, int target)
+    {
+        var currentNode = tree;
+        var closestValue = 0;
+        int currentDifference = 0;
+        int MinDifference = int.MaxValue;
+        while(currentNode!=null)
+        {
+            currentDifference=Math.Abs(currentNode.value - target);
+            if(currentDifference<MinDifference)
+            {
+                MinDifference = currentDifference;
+                closestValue = currentNode.value;
+            }
+            if(currentNode.value > target)
+            {
+                currentNode = currentNode.left;
+            }
+            else
+            {
+                currentNode = currentNode.right;
+            }
+        }
+        return closestValue;
+    }
+    
+   
+
+
+
+
+
+    public class BST
+    {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value)
+        {
+            this.value = value;
+        }
+    }
+
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
