96af41c681d7ee14f2230ca0199ae97ab6876a8e
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-09-01 22:20:02 +0530

    Add solution for Pallindrome Check

---
+++ String/Easy/Pallindrom/ConsoleApp1/Pallindrome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Pallindrome
+{
+    public static bool IsPallindrome(string str)
+    {
+        char[] charArr=str.ToCharArray();
+        string revStr = "";
+        for (int i = charArr.Length - 1; i >= 0; i--)
+        {
+            revStr=revStr + charArr[i];
+        }
+        if(str==revStr)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    public static void Main(string[] args)
+    {
+        bool result = IsPallindrome("abcdcb");
+        Console.WriteLine(result);
+    }
+}
