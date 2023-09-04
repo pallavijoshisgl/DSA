35dc37c7165251737d66c37230beccb6d0ba72cb
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-09-04 23:09:12 +0530

    Add code for ValidateSubsequence

---
+++ Graphs/Easy/Depth_First_Search/Depth_First_Search.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+public class Node
+{
+    public string name;
+    public List<Node> children;
+
+    public Node(string name)
+    {
+        this.name = name;
+    }
+
+    public Node AddChild(string name)
+    {
+        Node node = new Node(name);
+        children.Add(node);
+        return this;
+    }
+
+    public  List<string> DepthFirstSearch(List<string> array)
+    {
+        array.Add(name);
+        foreach(Node child in children)
+        {
+            child.DepthFirstSearch(array);
+        }
+        return array;
+    }
+
+    public static void Main(string[] args)
+    {
+        Node node = new Node("A");
+        
+    }
+}
