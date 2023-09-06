5787ae175401f0c5f8a6c7aa2f10114f949e8da6
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-09-06 23:57:15 +0530

    Add code for Best Seat Problem

---
+++ Arrays/Medium/BestSeat/Best_Seat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Best_Seat
+{
+    public int FindBestSeat(int[] seats)
+    {
+
+        int maxSpace = 0;
+        int availableSpace = 0;
+        int BestSeat = -1;
+        int j = 0;
+        for(int i=0; i<seats.Length; i++)
+        {
+            j = i + 1;
+            while(j< seats.Length && seats[j]==0)
+            {
+                j++;
+            }
+            availableSpace = j - i - 1;
+            if(availableSpace > maxSpace)
+            {
+                maxSpace = availableSpace;
+                BestSeat = (j + i) / 2;
+                j = i;
+            }
+        }
+        return BestSeat;
+    }
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
