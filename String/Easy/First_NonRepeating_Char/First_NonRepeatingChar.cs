254dc627a72d1fdf44cc6d3db576523c942d6a26
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-09-11 23:34:09 +0530

    Add code for First Non Repeating Character

---
+++ String/Easy/First_NonRepeating_Char/First_NonRepeatingChar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class First_NonRepeatingChar
+{
+    public static int FirstNonRepeatingCharacter(string str)
+    {
+        int Length = str.Length;
+
+        for(int i=0; i<Length; i++)
+        {
+            char ch = str[i];
+            string sub = str.Remove(i, 1);
+            if(!sub.Contains(ch))
+            {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    public static void Main(string[] args)
+    {
+        string str = "abcdcaf";
+        Console.WriteLine(FirstNonRepeatingCharacter(str));
+
+    }
+}
