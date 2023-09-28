d8d963fceb76b626885cab2806f2f7604da9da13
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-09-28 21:06:45 +0530

    Add code for Binary Search

---
+++ Searching/Easy/BinarySearch/Binary_Search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+public class Binary_Search
+{
+    public static int BinarySearch(int[] array, int target)
+    {
+        int l = 0;
+        int h = array.Length - 1;
+
+        while(l<=h)
+        {
+            int mid = l + (h - l) / 2;
+            if(array[mid]==target)
+            {
+                return mid;
+            }
+            else if(array[mid]>target)
+            {
+                h = mid - 1;
+            }
+            else if(array[mid]<target)
+            {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] array = new int[] { 1, 5, 7, 9, 10, 22, 33, 44 };
+        int target = 44;
+        Console.WriteLine(BinarySearch(array, target));
+    }
+}
