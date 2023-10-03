a74f3edfccf92886302d5431b66e2985281a124f
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-10-03 15:26:19 +0530

    Add code for Node Depth of a Tree

---
+++ Trees/Easy/Node_Depths/GetNodeDepth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class GetNodeDepth
+{
+    public static int NodeDepths(BinaryTree root,int depth=0)
+    {
+        return root != null ? depth + NodeDepths(root.left, depth + 1) + NodeDepths(root.right, depth + 1) : 0;
+    }
+
+    public class BinaryTree
+    {
+        public int value;
+        public BinaryTree right;
+        public BinaryTree left;
+
+
+    }
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
