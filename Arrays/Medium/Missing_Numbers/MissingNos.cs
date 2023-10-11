2cdc4fd60386849d74cd569f1f48f8889e58a818
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-10-11 23:09:22 +0530

    Add code for Missing Numbers

---
+++ Arrays/Medium/Missing_Numbers/MissingNos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Missing_Numbers
+{
+    public class MissingNos
+    {
+
+        public static int[] MissingNumbers(int[] nums)
+        {
+            var result = new List<int>();
+            var expectedTotal = nums.Length + 2;
+
+            for(int i=1;i<=expectedTotal;i++)
+            {
+                if (nums.Contains(i)) continue;
+                else
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result.ToArray();
+        }
+        static void Main(string[] args)
+        {
+
+            int[] nums = new int[] { 1, 3, 4, 5 };
+            int[] result= MissingNumbers(nums);
+
+            for(int i=0;i<result.Length;i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
