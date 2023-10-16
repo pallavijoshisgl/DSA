4ee7dc7539a27121107f2bc7ec1b2081cca8eebb
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-10-16 20:47:25 +0530

    Add code for Array Of Products

---
+++ Arrays/Medium/Array_Of_Products/ArrayOfProducts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class ArrayOfproducts
+{
+    public int[] ArrayOfProducts(int[] array)
+    {
+        
+        int[] productArray = new int[array.Length];
+        
+
+        int leftRunningpPoduct = 1;
+        for(int i = 0; i<array.Length; i++)
+        {
+            productArray[i] = leftRunningpPoduct;
+            leftRunningpPoduct = leftRunningpPoduct * array[i];
+        }
+
+        int rightRunningProduct = 1;
+        for (int j = array.Length - 1; j >= 0;j--)
+        {
+            productArray[j] = productArray[j] * rightRunningProduct;
+            rightRunningProduct = rightRunningProduct * array[j];
+        }
+
+        return productArray;
+    }
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
