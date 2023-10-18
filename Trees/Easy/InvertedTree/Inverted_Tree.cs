fdbb92590646c6fec5509eb3f660da47b49fa615
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-10-18 21:53:03 +0530

    Add code for Inverted Tree

---
+++ Trees/Easy/InvertedTree/Inverted_Tree.cs
@@ -0,0 +1,51 @@
+﻿
+
+public class Inverted_Tree
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val= val;
+            this.left= left;
+            this.right= right;
+        }
+    }
+    public static TreeNode InvertTree(TreeNode root)
+    {
+        if(root == null)
+            return null;
+
+        TreeNode invertedLeft = InvertTree(root.left);
+        TreeNode invertedRight = InvertTree(root.right);
+
+        root.right = invertedLeft;
+        root.left = invertedRight;
+
+        return root;
+    }
+
+    public static void Main(string[] args)
+    {
+        TreeNode root = new TreeNode();
+        root.val = 4;
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+
+        TreeNode result = InvertTree(root);
+       
+
+    }
+}
