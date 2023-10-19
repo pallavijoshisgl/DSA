06d3ac597ee9c982939122ec3d1193118dce2f55
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-10-19 22:32:15 +0530

    Add code for Longest consecutive subsequence

---
+++ Arrays/Medium/ConsoleApp1/Longest_Cons_Sub.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+public class Longest_Cons_Sub
+{
+    public static int Get_longest_Con_Subsequence(int[] nums)
+    {
+        if(nums.Length < 2)
+        {
+            return nums.Length;
+        }
+
+        Array.Sort(nums);
+
+        int max_Length = 1;
+        int currentLength = 1;
+
+        for(int i=1;i<nums.Length;i++)
+        {
+            if(nums[i]-nums[i-1] == 1)
+            {
+                currentLength++;
+            }
+            else if(nums[i]==nums[i-1])
+            {
+                continue;
+            }
+            else
+            {
+                currentLength = 1;
+            }
+
+            max_Length = Math.Max(max_Length, currentLength);
+        }
+
+        return max_Length;
+    }
+
+
+    public static void Main(string[] args)
+    {
+        int[] nums = new int[] { 100, 4, 200, 1, 3, 2 };
+        int result=Get_longest_Con_Subsequence(nums);
+        Console.WriteLine(result);
+    }
+
+}
