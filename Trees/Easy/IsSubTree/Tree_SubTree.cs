18a05e397f2b8e5be48c8a67b657ff0a8f339714
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-10-23 12:47:37 +0530

    Add code for SubTree of Another Tree Problem

---
+++ Trees/Easy/IsSubTree/Tree_SubTree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+public class Tree_SubTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val=0, TreeNode left=null,TreeNode right=null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static bool IsSubTree(TreeNode root, TreeNode subRoot)
+    {
+        if(subRoot == null)
+        {
+            return true;
+        }
+        if(root == null)
+        {
+            return false;
+        }
+
+        if (IsSameTree(root, subRoot)) return true;
+
+        return(IsSubTree(root.left, subRoot) || IsSubTree(root.right, subRoot));
+    }
+
+    public static bool IsSameTree(TreeNode root,TreeNode subRoot)
+    {
+        if (root == null && subRoot == null)
+            return true;
+
+        if (root!=null && subRoot!=null && root.val == subRoot.val && IsSameTree(root.left,subRoot.left) && IsSameTree(root.right,subRoot.right))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
