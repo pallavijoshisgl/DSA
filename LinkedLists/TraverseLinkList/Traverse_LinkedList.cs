d4540d2e07523f78581c18b8c6a738c2a87ee8ed
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-10-28 12:39:41 +0530

    Add code for traversing a linkedlist

---
+++ LinkedLists/TraverseLinkList/Traverse_LinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraverseLinkList
+{
+    public class Traverse_LinkedList
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int value)
+            {
+                val= value;
+            }
+        }
+
+        public static void TraverseLinkedList(ListNode Head)
+        {
+            ListNode temp = Head;
+            while(temp != null)
+            {
+                int val = temp.val;
+                Console.WriteLine(val);
+                temp = temp.next;
+            }
+            Console.ReadKey();
+        }
+        public static void Main(string[] args)
+        {
+            ListNode l1 = new ListNode(4);
+            ListNode l2 = new ListNode(8);
+            ListNode l3 = new ListNode(13);
+
+            l1.next = l2;
+            l2.next = l3;
+            l3.next = null;
+            ListNode head = l1;
+            TraverseLinkedList(head);
+        }
+    }
+}
