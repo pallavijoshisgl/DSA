50857a9a00be92f20128dc3ee459c018d4eb54b0
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-10-30 23:55:42 +0530

    Add code for Remove Duplicates in Linked Lists

---
+++ LinkedLists/RemoveDuplicates/Remove_Duplicates.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    public class Remove_Duplicates
+    {
+
+        public class LinkedList
+        {
+            public int value;
+            public LinkedList next;
+
+            public LinkedList(int value)
+            {
+                this.value = value;
+                this.next = null;
+            }
+        }
+
+        public LinkedList RemoveDuplicatesFromLinkedLists(LinkedList linkedList)
+        {
+
+            LinkedList curr = linkedList;
+            while (curr != null && curr.next != null)
+            {
+                if(curr.value == curr.next.value)
+                {
+                    curr.next = curr.next.next;
+                }
+                else
+                {
+                    curr = curr.next;
+                }
+            }
+
+            return linkedList;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
