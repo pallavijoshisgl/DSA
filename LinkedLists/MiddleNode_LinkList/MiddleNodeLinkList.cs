6b96e3ac8f0386a5e6fd8777d11724e7a75345cf
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-10-31 23:47:39 +0530

    Add code for Middle Node in Linked List

---
+++ LinkedLists/MiddleNode_LinkList/MiddleNodeLinkList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleNode_LinkList
+{
+    public class MiddleNodeLinkList
+    {
+
+        public class LinkedList
+        {
+            public int value;
+            public LinkedList next;
+
+            public LinkedList(int value)
+            {
+                this.value = value;
+                this.next = null;
+            }
+        }
+
+        public LinkedList MiddleNode(LinkedList linkedList)
+        {
+            LinkedList slowNode = linkedList;
+            LinkedList fastNode = linkedList;
+            while (slowNode != null && fastNode !=null)
+            {
+                slowNode = slowNode.next;
+                fastNode = fastNode.next.next;
+            }
+
+            return slowNode;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
