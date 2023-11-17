34e881a12c4f817f23185815d62ee31442414933
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-11-17 12:35:52 +0530

    Add code for Recursion Nth Fibonacci

---
+++ Recursion/Easy/ConsoleApp1/Recursion_Prb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionProblem
+{
+    internal class Recursion_Prb
+    {
+
+        public static int GetNthFib(int n)
+        {
+            if(n<=2)
+            {
+                return n - 1;
+            }
+
+            return GetNthFib(n-1) + GetNthFib(n-2);
+        }
+        static void Main(string[] args)
+        {
+            int n = 6;
+            int result=GetNthFib(n);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
