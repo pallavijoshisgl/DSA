92132826e6182c764921a5f508975b3473b7ba59
Pallavi Joshi <pallavijoshisgl@gmail.com>
2023-11-22 21:42:30 +0530

    Add code for BST Traversal

---
+++ Trees/Easy/BSTTraversal/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTTraversal
+{
+    internal class Program
+    {
+        public static List<int> InOrderTraversal(BST tree, List<int> array)
+        {
+            if(tree == null)
+            {
+                return array;
+            }
+
+            InOrderTraversal(tree.left, array);
+            array.Add(tree.value);
+            InOrderTraversal(tree.right, array);
+
+            return array;
+        }
+
+        public static List<int> PreOrderTraversal(BST tree, List<int> array)
+        {
+            if(tree == null)
+            {
+                return array;
+            }
+
+            array.Add(tree.value);
+            PreOrderTraversal(tree.left, array);
+            PreOrderTraversal(tree.right, array);
+
+            return array;
+        }
+
+        public static List<int> PostOrderTraversal(BST tree, List<int> array)
+        {
+            if(tree == null)
+            {
+                return array;
+            }
+
+            PostOrderTraversal(tree.left, array);
+            PostOrderTraversal(tree.right, array);
+            array.Add(tree.value);
+
+            return array;
+        }
+
+        public class BST
+        {
+            public int value;
+            public BST left;
+            public BST right;
+
+            public BST(int value)
+            {
+                this.value = value;
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
