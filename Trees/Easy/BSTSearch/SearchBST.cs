749e5ff94de199d5a98895b9f48875d983d0ed4e
Pallavi Joshi <pallavijoshisgl@gmail.com>
2024-02-06 13:03:56 +0530

    Add code for Search in BST

---
+++ Trees/Easy/BSTSearch/SearchBST.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTSearchCode
+{
+    internal class SearchBST
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+        public TreeNode SearchINBST(TreeNode root, int val)
+        {
+            if(root == null)
+                return null;
+
+            if (root.val == val)
+            {
+                return root;
+            }
+            else if (root.val < val)
+            {
+                return SearchINBST(root.right, val);
+            }
+            else
+            {
+                return SearchINBST(root.left, val);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
