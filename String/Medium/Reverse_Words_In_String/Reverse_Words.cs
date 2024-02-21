0824119c1b7bbc02cff3ff6112665a9ec39ee01a
Pallavi Joshi <pallavijoshisgl@gmail.com>
2024-02-21 13:16:35 +0530

    Add code for Reverse Words In Input String

---
+++ String/Medium/Reverse_Words_In_String/Reverse_Words.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Words_In_String
+{
+    internal class Reverse_Words
+    {
+        public static string Reverse_Words_In_input(string inputStr)
+        {
+            string reverseWords = "";
+            string word = "";
+
+            for(int i=0;i<inputStr.Length;i++)
+            {
+                if(inputStr[i] == ' ')
+                {
+                    word = inputStr[i] + word;
+                    reverseWords = word + reverseWords;
+                    word = "";
+                }
+                else
+                {
+                    word=word + inputStr[i];
+                }
+            }
+            reverseWords = word + reverseWords;
+            return reverseWords;
+
+        }
+        static void Main(string[] args)
+        {
+            string inputStr = "Tim is great!";
+            string result= Reverse_Words_In_input(inputStr);
+            Console.WriteLine(result);
+            Console.ReadKey();
+                
+        }
+    }
+}
